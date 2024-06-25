bce48d77d3d212e5b4e0a1e31c3ba767ab05cdd5
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2024-06-24 23:56:09 -0400

    add workouts

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 slint::include_modules!();
+use std::rc::Rc;
 
 fn main() -> Result<(), slint::PlatformError> {
+    use slint::Model;
+
     let ui = AppWindow::new()?;
 
-    // ui.on_add_reps({
-    //     let ui_handle = ui.as_weak();
-    //     move |reps| {
-    //         let ui = ui_handle.unwrap();
-    //         ui.set_rep_count(ui.get_rep_count() + reps);
-    //     }
-    // });
+    ui.on_add_cali({
+        let ui_handle = ui.as_weak();
+        move |cali_data| {
+            let ui = ui_handle.unwrap();
+
+            let mut cali_counters: Vec<CaliData> = ui.get_cali_counters().iter().collect();
+            cali_counters.push(cali_data);
+
+            let cali_counters_model = Rc::new(slint::VecModel::from(cali_counters));
+            ui.set_cali_counters(cali_counters_model.into());
+        }
+    });
+
+    // let mut cali_counters: Vec<CaliData> = ui.get_cali_counters().iter().collect();
+
+    // for cali_counter in &cali_counters {
+    //     //
+    // }
 
     ui.run()
 }
